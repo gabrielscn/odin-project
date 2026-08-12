8009578a7df1b74be5367d9f080921bb7ba0264b
gabrielscn <gnetosoares@gmail.com>
2026-08-11 23:19:22 -0300

    ...

--- library/script.js
+++ library/script.js
@@ -1,7 +1,12 @@
 const myLibrary = [];
 
-function Book() {
-
+class Book {
+  constructor(title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+  }
 }
 
 function addBookToLibrary() {
