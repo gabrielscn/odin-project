b5d8f8624152d76f96849b85ade96fc617382b3e
gabrielscn <gnetosoares@gmail.com>
2026-08-12 23:42:24 -0300

    ...

--- library/script.js
+++ library/script.js
@@ -20,3 +20,51 @@ function removeItem(id) {
   myLibrary.splice(itemToDelete.id, 1);
   handleStorageAndDisplay();
 }
+
+function handleStorageAndDisplay() {
+  localStorage.setItem('books', JSON.stringify(myLibrary));
+  populateLibrary(myLibrary, itemList);
+}
+
+function toggleStatus(e) {
+  if (e.target.classList.contains('read-status')) {
+    const index = e.target.parentNode.id;
+    if (myLibrary[index].read === 'read') {
+      myLibrary[index].read = 'not read'
+    } else {
+      myLibrary[index].read = 'read';
+    }
+    handleStorageAndDisplay();
+  }
+}
+
+function addBookToLibrary(e) {
+  e.preventDefault();
+  if (bookTitle.value === '' || bookAuthor.value === '' || readStatus.value === '') {
+    alert(`Please fill the form!`);
+    return;
+  } else {
+    const newItem = new Book(bookTitle.value, bookAuthor.value, readStatus.value);
+    myLibrary.push(newItem);
+    handleStorageAndDisplay();
+    form.reset();
+  }
+}
+
+function populateLibrary(myLibrary, itemList) {
+  itemList.innerHTML = myLibrary.map(
+    (book, id) => {
+      return `<tr class="table-item" id="${id}">
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td class="read-status">${book.read}</td>
+      <td><button class="table-item-button" id="${id}" onclick="removeItem(${id})">delete</button>
+      </tr>`
+    })
+    .join('');
+}
+
+populateLibrary(myLibrary, itemList);
+
+form.addEventListener('submit', addBookToLibrary);
+itemList.addEventListener('click', toggleStatus);
