0a24ed7ff25ac8d6942647e6dcaa0092e07809bf
gabrielscn <gnetosoares@gmail.com>
2026-08-17 23:59:23 -0300

    ...

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -35,6 +35,16 @@ const Game = (() => {
     currentPlayerIndex = 0;
     gameOver = false;
     Gameboard.render();
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      square.addEventListener("click", handleClick);
+    })
+
+  }
+
+  const handleClick = (event) => {
+    let index = parseInt(event.target.id.split("-")[1]);
+    alert(index);
   }
 
   return {
